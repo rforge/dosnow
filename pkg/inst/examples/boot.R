0b0587a2a2767526444a80d98aa801e8042fd59c
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-27 01:49:32 +0000

    Modified to include a large amount of data in order to see the affect of attach/detach.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dosnow@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/boot.R
+++ pkg/inst/examples/boot.R
@@ -2,11 +2,16 @@ library(doSNOW)
 cl <- makeSOCKcluster(4)
 registerDoSNOW(cl)
 
+junk <- matrix(0, 1000000, 8)
+print(object.size(junk))
+
 x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+
 trials <- 10000
 
 ptime <- system.time({
-  r <- foreach(icount(trials), .combine=cbind) %dopar% {
+  r <- foreach(icount(trials), .combine=cbind,
+               .export='junk') %dopar% {
     ind <- sample(100, 100, replace=TRUE)
     result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
     coefficients(result1)
@@ -18,7 +23,7 @@ print(ptime)
 ptime2 <- system.time({
   snowopts <- list(preschedule=TRUE)
   r <- foreach(icount(trials), .combine=cbind,
-               .options.snow=snowopts) %dopar% {
+               .export='junk', .options.snow=snowopts) %dopar% {
     ind <- sample(100, 100, replace=TRUE)
     result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
     coefficients(result1)
@@ -29,7 +34,8 @@ print(ptime2)
 
 ptime3 <- system.time({
   chunks <- getDoParWorkers()
-  r <- foreach(n=idiv(trials, chunks=chunks), .combine=cbind) %dopar% {
+  r <- foreach(n=idiv(trials, chunks=chunks), .combine=cbind,
+               .export='junk') %dopar% {
     y <- lapply(seq_len(n), function(i) {
       ind <- sample(100, 100, replace=TRUE)
       result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
@@ -42,15 +48,16 @@ cat('chunked parallel foreach:\n')
 print(ptime3)
 
 ptime4 <- system.time({
-  mkworker <- function(x) {
+  mkworker <- function(x, junk) {
     force(x)
+    force(junk)
     function(i) {
       ind <- sample(100, 100, replace=TRUE)
       result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
       coefficients(result1)
     }
   }
-  y <- parLapply(cl, seq_len(trials), mkworker(x))
+  y <- parLapply(cl, seq_len(trials), mkworker(x, junk))
   r <- do.call('cbind', y)
 })[3]
 cat('parLapply:\n')
@@ -66,5 +73,15 @@ stime <- system.time({
 })[3]
 cat('sequential lapply:\n')
 print(stime)
+
+stime2 <- system.time({
+  r <- foreach(icount(trials), .combine=cbind) %do% {
+    ind <- sample(100, 100, replace=TRUE)
+    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+    coefficients(result1)
+  }
+})[3]
+cat('sequential foreach:\n')
+print(stime2)
 
 stopCluster(cl)
