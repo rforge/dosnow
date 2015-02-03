df876ce17b1717c5b7e1bb5c1d174fa59dc2796b
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-03 20:37:04 +0000

    Minor improvements.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dosnow@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- progress.R
+++ progress.R
@@ -4,34 +4,44 @@ library(tcltk)
 cl <- makeSOCKcluster(3)
 registerDoSNOW(cl)
 
-pb <- tkProgressBar(max=100)
-progress <- function(n) setTkProgressBar(pb, n)
+pb <- txtProgressBar(max=100, style=3)
+progress <- function(n) setTxtProgressBar(pb, n)
 opts <- list(progress=progress)
 r <- foreach(i=1:100, .options.snow=opts) %dopar% {
-  Sys.sleep(3)
+  Sys.sleep(1)
+  sqrt(i)
+}
+close(pb)
+
+pb <- tkProgressBar(max=100, title="doSNOW Progress Bar Test")
+progress <- function(n, tag) setTkProgressBar(pb, n,
+                label=sprintf("last completed task: %d", tag))
+opts <- list(progress=progress)
+r <- foreach(i=1:100, .options.snow=opts) %dopar% {
+  Sys.sleep(1)
   sqrt(i)
 }
 close(pb)
 
 progress <- function() cat('.')
 opts <- list(progress=progress)
-r <- foreach(i=1:100, .options.snow=opts) %dopar% {
-  Sys.sleep(3)
+r <- foreach(i=1:50, .options.snow=opts) %dopar% {
+  Sys.sleep(0.5)
   sqrt(i)
 }
 cat('\n')
 
 progress <- function(n, tag) cat(sprintf('Task %d completed\n', tag))
 opts <- list(progress=progress)
-r <- foreach(i=1:100, .options.snow=opts) %dopar% {
-  Sys.sleep(i %% 10)
+r <- foreach(i=1:10, .options.snow=opts) %dopar% {
+  Sys.sleep(10 - i)
   sqrt(i)
 }
 
 progress <- function(n, tag) stop(tag)
 opts <- list(progress=progress)
-r <- foreach(i=1:100, .options.snow=opts) %dopar% {
-  Sys.sleep(i %% 10)
+r <- foreach(i=1:10, .options.snow=opts) %dopar% {
+  Sys.sleep(10 - i)
   sqrt(i)
 }
 
